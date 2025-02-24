7852d56ed0ecd7eeea65454e725bd96459cf5efd
Krishnaiah-003 <gvenkatakrishnaiah@softpathtech.com>
2025-02-24 12:10:45 +0530

    close code is added

--- src/test/java/Google/Open_Google.java
+++ src/test/java/Google/Open_Google.java
@@ -4,20 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Open_Google {
+	WebDriver driver;
 
 	@Test
 	public void Google_Jenkins() {
-		
-	WebDriverManager.chromedriver().setup();
-	WebDriver driver =new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("https://www.google.co.in/");
-	driver.findElement(By.id("APjFqb")).sendKeys("Krishna",Keys.ENTER);
-	driver.quit();
+
+		WebDriverManager.chromedriver().setup();
+		driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in/");
+		driver.findElement(By.id("APjFqb")).sendKeys("Krishna",Keys.ENTER);
+	}
+	@AfterTest
+	public void close() {
+		driver.quit();
+
 	}
 }
