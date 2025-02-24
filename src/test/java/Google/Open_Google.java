7c7ec0059f5b953280fa7d5f726755af36febd46
Krishnaiah-003 <gvenkatakrishnaiah@softpathtech.com>
2025-02-24 13:08:57 +0530

    change the code and added

--- src/test/java/Google/Open_Google.java
+++ src/test/java/Google/Open_Google.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,14 +13,26 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Open_Google {
 	WebDriver driver;
 
-	@Test
-	public void Google() {
+	@BeforeTest
+	public void NavigateToUrl() throws Exception {
 
 		WebDriverManager.chromedriver().setup();
 		driver =new ChromeDriver();
 		driver.manage().window().maximize();
+		Thread.sleep(1000);
 		driver.get("https://www.google.co.in/");
+		Thread.sleep(2000);
+	}
+	@Test(priority=1)
+	public void Google() {
 		driver.findElement(By.id("APjFqb")).sendKeys("Krishna",Keys.ENTER);
+	}
+	@Test(priority=2)
+	public void Gmail() throws Exception {
+		driver.navigate().back();
+		driver.findElement(By.id("APjFqb")).clear();
+		Thread.sleep(1000);
+		driver.findElement(By.id("APjFqb")).sendKeys("Gmail",Keys.ENTER);
 	}
 	@AfterTest
 	public void close() {
